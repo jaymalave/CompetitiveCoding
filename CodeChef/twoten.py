bf40709f8616f5107d0781ee0b26e6a77e5d8e41
Jay Malave <73488443+jaymalave@users.noreply.github.com>
2021-07-09 00:36:25 +0530

    fixed syntax

--- CodeChef/twoten.py
+++ CodeChef/twoten.py
@@ -3,11 +3,11 @@ k = int(input())
 for i in range(0, k):
     x = int(input())
 
-    if (x%2 == 0 or x%4 == 0 or x%6 == 0 or x%8 == 0):
+    if (x%2 == 0 or x%4 == 0 or x%6 == 0 or x%8 == 0 and x%10 != 0):
         print('5')
-    if (x%5 == 0 and x%10 != 0):
+    elif (x%5 == 0 and x%10 != 0):
         print('1')
-    if (x%10 == 0 and x%5 != 0):
+    elif (x%10 == 0):
         print(0)
     else:
         print('-1')
