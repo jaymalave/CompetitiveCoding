a7199efd06651c92e03461f84d1e7c35616dfba0
jaymalave <jaym.github@gmail.com>
2021-10-18 15:37:29 +0530

    rectified maxfill solution

--- CodeChef/maxfill.py
+++ CodeChef/maxfill.py
@@ -2,6 +2,8 @@ count = 0
 actual_list = []
 
 def maxIndex(lst):
+    if len(lst) == 0:
+        return 0
     max_val = max(lst)
     max_index = lst.index(max_val)
     return max_index
@@ -9,13 +11,13 @@ def maxIndex(lst):
 def findMaxAndFill(newA, m):
     global count
     global actual_list
-    
-    if m == actual_list[-1]:
+
+    if m == 0:
        return count
     
     for i in range(0, m):
         count += newA[m] - newA[i]
-    return findMaxAndFill(newA[m+1:None], maxIndex(newA[m+1:None]))
+    return findMaxAndFill(newA[m+1:], maxIndex(newA[m+1:]))
 
 actual_list = [2,3,0,2,0,4,0,3,0,1]
 print(findMaxAndFill(actual_list, maxIndex(actual_list)))
