0b19dca10d63790719b91a2c26f2c5b7edcca46f
jaymalave <jaym.github@gmail.com>
2021-10-19 12:17:59 +0530

    maxfill updated to take inputs

--- CodeChef/maxfill.py
+++ CodeChef/maxfill.py
@@ -1,6 +1,14 @@
 count = 0
 actual_list = []
 
+
+def inputList():
+    n = int(input())
+    for i in range(0, n):
+        a = int(input())
+        actual_list.append(a)
+    return actual_list
+
 def maxIndex(lst):
     if len(lst) == 0:
         return 0
@@ -19,5 +27,5 @@ def findMaxAndFill(newA, m):
         count += newA[m] - newA[i]
     return findMaxAndFill(newA[m+1:], maxIndex(newA[m+1:]))
 
-actual_list = [2,3,0,2,0,4,0,3,0,1]
+actual_list = inputList()
 print(findMaxAndFill(actual_list, maxIndex(actual_list)))
