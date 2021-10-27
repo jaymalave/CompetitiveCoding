ee3c9ede318d621d73bdb71168717348a13706ae
jaymalave <jaym.github@gmail.com>
2021-10-27 13:13:38 +0530

    maxfill soln with a better complexity

---
+++ CodeChef/updatedmf.py
@@ -0,0 +1,20 @@
+def maxFill(arr, n):
+	fill = 0
+	
+	for i in range(1, n-1):
+		left = arr[i];
+		for j in range(i):
+			left = max(left, arr[j])
+			
+		right = arr[i]
+		for j in range(i+1 , n):
+			right = max(right, arr[j])
+			
+		fill = fill + (min(left, right) - arr[i])
+
+	return fill
+
+
+arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+print(maxFill(arr, len(arr)))
+	
